b2eebd512a18ed3c546fbda10693f0e1ee78b967
Blue <ericmager@gmx.de>
2017-01-24 22:46:19 +0100

    Create accountsys_server.lua

---
+++ accountsys_server.lua
@@ -0,0 +1,50 @@
+generalPW = "UjF2kVPW"
+
+
+function logPlayerIn(thePlayer,hash)
+	if thePlayer and hash then
+		if getAccount(getPlayerName(thePlayer)) then
+			if hash == getAccountData(getAccount(getPlayerName(thePlayer)), "user.password") then
+				logIn(thePlayer, getAccount(getPlayerName(thePlayer)),generalPW)
+				triggerClientEvent(thePlayer, "loginSuccess",resourceRoot,hash)
+			end
+		end
+	end
+end
+addEvent("logPlayerIn",true)
+addEventHandler("logPlayerIn", root,logPlayerIn)
+
+
+function tryLoginorRegister(thePlayer,hash)
+	if thePlayer and hash then
+		if getAccount(getPlayerName(thePlayer)) then
+			if hash == getAccountData(getAccount(getPlayerName(thePlayer)), "user.password") then
+				logIn(thePlayer, getAccount(getPlayerName(thePlayer)),generalPW)
+				triggerClientEvent(thePlayer, "loginSuccess",resourceRoot,hash)
+			end
+		else
+		acc = addAccount(getPlayerName(thePlayer),generalPW)
+		setAccountData(acc,"user.password", hash)
+			logPlayerIn(thePlayer,UjF2kVPW)
+		end
+	end
+	end
+addEvent("tryLoginorRegister",true)
+addEventHandler("tryLoginorRegister",root,tryLoginorRegister)
+
+
+
+
+function checkforcommands(command)
+	if command == "login" then
+	cancelEvent()
+	end
+	if command == "logout" then
+	cancelEvent()
+	kickPlayer(source, "Logging Out")
+	end
+
+
+
+end
+addEventHandler("onPlayerCommand", root, checkforcommands)
